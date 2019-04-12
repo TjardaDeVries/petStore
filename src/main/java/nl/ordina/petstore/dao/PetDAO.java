a882da4fb0281defac47e0b6f1a4c602fcf5f4fc
Tjarda de Vries <>
2019-04-12 15:04:01 +0200

    removes annotations

--- src/main/java/nl/ordina/petstore/dao/PetDAO.java
+++ src/main/java/nl/ordina/petstore/dao/PetDAO.java
@@ -1,8 +1,5 @@
 package nl.ordina.petstore.dao;
 
-import org.springframework.stereotype.Repository;
-
-@Repository
 public class PetDAO
 {
     @Override
--- src/main/java/nl/ordina/petstore/services/PetService.java
+++ src/main/java/nl/ordina/petstore/services/PetService.java
@@ -1,15 +1,11 @@
 package nl.ordina.petstore.services;
 
 import nl.ordina.petstore.dao.PetDAO;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
-@Service
 public class PetService
 {
     private final PetDAO petDAO;
 
-    @Autowired
     public PetService(PetDAO petDAO) {
         this.petDAO = petDAO;
     }
