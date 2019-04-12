2ee60f387384a49b093975a511e775d005335a15
Tjarda de Vries <>
2019-04-12 14:34:51 +0200

    replaces @Component with @Repository

--- src/main/java/nl/ordina/petstore/dao/PetDAO.java
+++ src/main/java/nl/ordina/petstore/dao/PetDAO.java
@@ -1,8 +1,8 @@
 package nl.ordina.petstore.dao;
 
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
-@Component
+@Repository
 public class PetDAO
 {
     @Override
