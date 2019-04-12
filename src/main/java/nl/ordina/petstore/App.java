15e13a422d6ae76758ba729eb75147c0bb1ea15c
Tjarda de Vries <>
2019-04-12 10:08:39 +0200

    initial commit

---
+++ src/main/java/nl/ordina/petstore/App.java
@@ -0,0 +1,13 @@
+package nl.ordina.petstore;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
